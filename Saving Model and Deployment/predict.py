3ad299a2e53bec10c7e0f70bcf2c304165d37e51
Mustapha Alaba <alabamustapha@gmail.com>
2023-11-02 22:39:47 +0100

    making predict file for churn model

--- Saving Model and Deployment/predict.py
+++ Saving Model and Deployment/predict.py
@@ -1,35 +1,33 @@
 
+from flask import Flask, request, jsonify
 import pickle 
 
+
+
+app = Flask('ping')
 input_model = "model_C=1.0.bin"
 
 with open(input_model, 'rb') as f_in:
     dv, model  = pickle.load(f_in)
 
+@app.route('/predict', methods=['POST'])
+def predict():
 
-customer_dict = {'customerid': '0111-klbqg',
- 'gender': 'male',
- 'seniorcitizen': 1,
- 'partner': 'yes',
- 'dependents': 'yes',
- 'tenure': 32,
- 'phoneservice': 'yes',
- 'multiplelines': 'no',
- 'internetservice': 'fiber_optic',
- 'onlinesecurity': 'no',
- 'onlinebackup': 'yes',
- 'deviceprotection': 'no',
- 'techsupport': 'no',
- 'streamingtv': 'yes',
- 'streamingmovies': 'yes',
- 'contract': 'month-to-month',
- 'paperlessbilling': 'yes',
- 'paymentmethod': 'mailed_check',
- 'monthlycharges': 93.95,
- 'totalcharges': 2861.45}
+    customer = request.get_json()
+    X = dv.transform(customer_dict)
+    predict_proba = model.predict_proba(X)[0, 1]
+
+    churn = predict_proba >= 0.5
+
+    result = {
+        'churn_probability': predict_proba
+        'churn': churn
+    }
+
+    return jsonify(result)
 
 
-X = dv.transform(customer_dict)
-predict_proba = model.predict_proba(X)[0, 1]
 
-print(f"Probability of churning: {predict_proba}")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9696, host='0.0.0.0')
